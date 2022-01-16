38c14b05ce7613d05363f7bb0e418d9351789370
atishjain9 <atis_jain@yahoo.com>
2022-01-16 11:05:16 +0530

    This is My Second Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Welcome to Java");
+        System.out.println("This is Saved to GIt...");
     }
 }
